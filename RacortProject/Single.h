ab96c423d6017f395af35a58885f8b70d31c8bff
Gabriel Vieira <viera_frifri@hotmail.com>
2014-09-03 15:08:46 -0300

    single
    single

---
+++ RacortProject/Single.h
@@ -0,0 +1,15 @@
+//
+//  Single.h
+//  RacortProject
+//
+//  Created by Gabriel Vieira on 03/09/14.
+//  Copyright (c) 2014 Gabriel Vieira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Single : NSObject
+
+@property NSArray *lista;
+
+@end
